6af0b958ce84ffa7b376bb76910128f45f2daa24
yangshen <uQES0irbJDs>
2019-06-25 14:16:48 +0800

    爬虫服务 提交

--- clean-service/src/main/java/shen/gd/cleanservice/controller/CallHelloController.java
+++ clean-service/src/main/java/shen/gd/cleanservice/controller/CallHelloController.java
@@ -15,11 +15,11 @@ public class CallHelloController {
 
     @RequestMapping("/call")
     public String call() {
-        ServiceInstance serviceInstance = loadBalancer.choose("service-producer");
+        ServiceInstance serviceInstance = loadBalancer.choose("service-exhibition");
         System.out.println("服务地址：" + serviceInstance.getUri());
         System.out.println("服务名称：" + serviceInstance.getServiceId());
 
-        String callServiceResult = new RestTemplate().getForObject(serviceInstance.getUri().toString() + "/hello", String.class);
+        String callServiceResult = new RestTemplate().getForObject(serviceInstance.getUri().toString() + "/evenBus", String.class);
         System.out.println(callServiceResult);
         return callServiceResult;
     }
--- clean-service/src/main/java/shen/gd/cleanservice/controller/ServiceController.java
+++ clean-service/src/main/java/shen/gd/cleanservice/controller/ServiceController.java
@@ -19,7 +19,7 @@ public class ServiceController {
      */
     @RequestMapping("/services")
     public Object services() {
-        return discoveryClient.getInstances("service-producer");
+        return discoveryClient.getInstances("service-exhibition");
     }
 
     /**
@@ -27,6 +27,6 @@ public class ServiceController {
      */
     @RequestMapping("/discover")
     public Object discover() {
-        return loadBalancer.choose("service-producer").getUri().toString();
+        return loadBalancer.choose("service-exhibition").getUri().toString();
     }
 }
